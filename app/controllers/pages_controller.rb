e0148386a418ecb7e58ccb2365350d847ef02323
Lucien George <luciengeorge95@gmail.com>
2024-02-26 10:19:34 +0000

    Authentication lecture done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :about]
+
   def home
+  end
+
+  def about
+  end
+
+  def profile
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
+
+  get :about, to: 'pages#about'
+  get :profile, to: 'pages#profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :users do
+  #   # ...
+  # end
 end
